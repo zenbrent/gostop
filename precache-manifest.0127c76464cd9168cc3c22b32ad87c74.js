1727c153adcd61fd48ee9c7b53c1971ec54b302f
Brent Brimhall <brent.brimhall@gmail.com>
2020-01-01 21:23:15 -0500

    Updates

---
+++ precache-manifest.0127c76464cd9168cc3c22b32ad87c74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e02db9ed552aa8708f4f2b124b04442",
+    "url": "/gostop/index.html"
+  },
+  {
+    "revision": "f1a7055c0438ec67ec0b",
+    "url": "/gostop/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fd081e3d706b6700554f",
+    "url": "/gostop/static/js/2.3a5718d3.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/gostop/static/js/2.3a5718d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1a7055c0438ec67ec0b",
+    "url": "/gostop/static/js/main.b84b5e08.chunk.js"
+  },
+  {
+    "revision": "fedf6a2a67420d75ed04",
+    "url": "/gostop/static/js/runtime-main.98c34789.js"
+  }
+]);
