3327294e1b5f2c7e4845586882d5da4d6d51294f
Brent Brimhall <brent.brimhall@gmail.com>
2020-01-01 20:35:41 -0500

    combo index

--- src/pages/Combinations/combinations.js
+++ src/pages/Combinations/combinations.js
@@ -10,6 +10,16 @@ const ComboPage = styled.div`
   grid-area: page;
 `;
 
+const ComboControls = styled.div`
+  grid-area: controls;
+  margin: 1rem;
+  margin-top: 0;
+`;
+
+const ComboLink = styled.a`
+  display: block;
+`;
+
 const Combination = styled.div`
   padding-bottom: 1rem;
 `;
@@ -41,90 +51,98 @@ const ComboPoints = styled(ComboTitle)`
 
 export function Combinations () {
   return (
-    <ComboPage>
-      {combinations.map(combination => {
-        const {
-          name,
-          pool: _pool = [],
-          count,
-          min,
-          requires = [],
-          excludes = [],
-          sets = [],
-          notes,
-          points
-        } = combination;
+    <>
+      <ComboControls>
+        {combinations.map(({ name }) => (
+          <ComboLink href={`#${name}`}>{name}</ComboLink>
+        ))}
+      </ComboControls>
 
-        const pool = pipe(
-          filter(x => !requires.includes(x)),
-          filter(x => !excludes.includes(x)),
-          uniq
-        ) (_pool);
+      <ComboPage>
+        {combinations.map(combination => {
+          const {
+            name,
+            pool: _pool = [],
+            count,
+            min,
+            requires = [],
+            excludes = [],
+            sets = [],
+            notes,
+            points
+          } = combination;
 
-        return (
-          <Combination>
-            <ComboTitle>
-              {name}
-            </ComboTitle>
-            <ComboPoints>
-              {points.base} {points.base === 1 ? 'point' : 'points'} 
-              {!points.additionalAfter
-                  ? ''
-                  : (
-                    ` after ${points.additionalAfter} cards, and an additional point per card after`
-                  )
-              }.
-            </ComboPoints>
-            {notes &&
-              <div>
-                Note: {notes}
-              </div>
-            }
-            {when(requires, () => (
-              <CardList>
-                <CardInstructions>
-                  {requires.length === 1 ? "this card" : "the following cards"}
-                </CardInstructions>
-                <CombinationCards cards={requires} />
-              </CardList>
-            ))}
-            {when(pool, () => (
-              <CardList>
-                <CardInstructions>
-                  {when(requires, () => "and")}
-                  {!min && !!pool.length && count && (count < pool.length + requires.length
-                    ? `any ${count - requires.length} of the following cards`
-                    : "all of the following cards")}
-                  {pool && min && `at least ${min} of the following cards`}
-                </CardInstructions>
-                <CombinationCards cards={pool.filter(c => !excludes.includes(c))} />
-              </CardList>
-            ))}
-            {when(excludes, () => (
-              <CardList>
-                <CardInstructions>
-                  not
-                </CardInstructions>
-                <CombinationCards cards={excludes} />
-              </CardList>
-            ))}
-            {when(sets, () => (
-              <CardList>
-                <CardInstructions>
-                  {sets.length === 1 && "All cards in this set"}
-                  {sets.length > 1 && "Any one of these sets"}
-                </CardInstructions>
-                {map(set => (
-                  <div>
-                    <CombinationCards cards={set} />
-                  </div>
-                )) (sets)}
-              </CardList>
-            ))}
-          </Combination>
-        );
-      })}
-    </ComboPage>
+          const pool = pipe(
+            filter(x => !requires.includes(x)),
+            filter(x => !excludes.includes(x)),
+            uniq
+          ) (_pool);
+
+          return (
+            <Combination>
+              <ComboTitle>
+                <a name={name}>{name}</a>
+              </ComboTitle>
+              <ComboPoints>
+                {points.base} {points.base === 1 ? 'point' : 'points'} 
+                {!points.additionalAfter
+                    ? ''
+                    : (
+                      ` after ${points.additionalAfter} cards, and an additional point per card after`
+                    )
+                }.
+              </ComboPoints>
+              {notes &&
+                <div>
+                  Note: {notes}
+                </div>
+              }
+              {when(requires, () => (
+                <CardList>
+                  <CardInstructions>
+                    {requires.length === 1 ? "this card" : "the following cards"}
+                  </CardInstructions>
+                  <CombinationCards cards={requires} />
+                </CardList>
+              ))}
+              {when(pool, () => (
+                <CardList>
+                  <CardInstructions>
+                    {when(requires, () => "and ")}
+                    {!min && !!pool.length && count && (count < pool.length + requires.length
+                      ? `any ${count - requires.length} of the following cards`
+                      : "all of the following cards")}
+                    {pool && min && `at least ${min} of the following cards`}
+                  </CardInstructions>
+                  <CombinationCards cards={pool.filter(c => !excludes.includes(c))} />
+                </CardList>
+              ))}
+              {when(excludes, () => (
+                <CardList>
+                  <CardInstructions>
+                    not
+                  </CardInstructions>
+                  <CombinationCards cards={excludes} />
+                </CardList>
+              ))}
+              {when(sets, () => (
+                <CardList>
+                  <CardInstructions>
+                    {sets.length === 1 && "All cards in this set"}
+                    {sets.length > 1 && "Any one of these sets"}
+                  </CardInstructions>
+                  {map(set => (
+                    <div>
+                      <CombinationCards cards={set} />
+                    </div>
+                  )) (sets)}
+                </CardList>
+              ))}
+            </Combination>
+          );
+        })}
+      </ComboPage>
+    </>
   );
 }
 
